14fae1a54c8804e6f4383373a12364b1310066ec
YannickArmspach <yannick.armspach@gmail.com>
2023-04-28 14:52:07 +0200

    remove dump

--- linotype/playgrounds/frontend/nuxt/src/linotype/block/nav/index.vue
+++ linotype/playgrounds/frontend/nuxt/src/linotype/block/nav/index.vue
@@ -2,7 +2,6 @@
   <div :id="`${props.blockType}-${props.blockId}`" :class="`block--${props.blockType}`" bg-gray-100 m-5 p-5>
     <div font-bold>[ Linotype block "{{props.blockType}}" / ref: {{props.blockData.reference}} ]</div>
     <div flex flex-row gap-2 py-2>
-      {{data}}
       <div v-for="menu, index in data" :key="index">
         <NuxtLink :to="menu?.slug">{{menu?.title}}</NuxtLink>
       </div>
