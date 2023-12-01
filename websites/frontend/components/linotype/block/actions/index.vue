ab32c9c97a8e0f7fd03eae37766abdaa65821b2f
YannickArmspach <yannick.armspach@gmail.com>
2023-12-01 16:23:40 +0100

    cleanup

--- websites/frontend/components/linotype/block/actions/index.vue
+++ websites/frontend/components/linotype/block/actions/index.vue
@@ -1,8 +1,5 @@
 <template>
   <div :id="`${props.blockType}-${props.blockId}`" :class="`block--${props.blockType}`" :data-reference="props.blockData.reference">
-    <LinotypeHelper :title="`BLOCK: ${props.blockType} / ID: ${props.blockId} / REF: ${props.blockData.reference}`" :data="props.blockData">
-      <!-- <LinotypeBlockActionsExample v-bind="props"/> -->
-    </LinotypeHelper>
     <!-- start coding here -->
   </div>
 </template>
