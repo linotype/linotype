ee9ccea3e1d45c1815f8477fc14b328cb418bd89
YannickArmspach <yannick.armspach@gmail.com>
2023-12-04 23:43:18 +0100

    website heroes

--- websites/frontend/components/linotype/block/heroes/index.vue
+++ websites/frontend/components/linotype/block/heroes/index.vue
@@ -6,27 +6,45 @@
         <div v-html="props.blockData?.title"/>
       </template>
       
-      <template #headline>
-      </template>
-      
       <template #description>
         <div v-html="props.blockData?.description"/>
       </template>
 
       <template #default>
         <LinotypeImage
-          v-if="props.blockData.background"
-          class="w-full"
+          v-if="props.blockData.image_dark"
+          class="hidden dark:block w-full"
           :sources="[
             {
-              source: props.blockData.background,
+              source: props.blockData.image_dark,
               size: [2000, 0],
               fit: 'cover',
               quality: 90,
               format: ['webp','jpeg']
             },
             {
-              source: props.blockData.background,
+              source: props.blockData.image_dark,
+              breakpoint: 800,
+              size: [800, 0],
+              fit: 'cover',
+              quality: 90,
+              format: ['webp','jpeg']
+            },
+          ]"
+        />
+        <LinotypeImage
+          v-if="props.blockData.image_light"
+          class="block dark:hidden w-full"
+          :sources="[
+            {
+              source: props.blockData.image_light,
+              size: [2000, 0],
+              fit: 'cover',
+              quality: 90,
+              format: ['webp','jpeg']
+            },
+            {
+              source: props.blockData.image_light,
               breakpoint: 800,
               size: [800, 0],
               fit: 'cover',
@@ -56,7 +74,8 @@ const props = defineProps<{
       link: string
       style: string
     }[]
-    background: object
+    image_light: object
+    image_dark: object
     fullheight: boolean
     offset: number
   };
