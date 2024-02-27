69ad466be0aaf81993c3ebfa93480a93f756d909
YannickArmspach <yannick.armspach@gmail.com>
2024-02-27 13:46:57 +0100

    better head

--- packages/@linotype/nuxt/src/runtime/components/linotype-template.vue
+++ packages/@linotype/nuxt/src/runtime/components/linotype-template.vue
@@ -10,37 +10,11 @@ import useLinotype from "./../composables/useLinotype"
 
 const { config, website, page, error } = useLinotype();
 
-const head_name = website.value?.name || ''
-const head_title = page.value?.seo?.title || website.value?.seo?.title || website.value?.name + ' - ' + page.value?.title || ''
-const head_description = page.value?.seo?.description || website.value?.seo?.description || '';
-const head_image = page.value?.seo?.image ? config.public.linotype.backend_url + '/assets/' + page.value.seo.image + '?width=1500&height=780' : ''
-                || website.value?.seo?.image ? config.public.linotype.backend_url + '/assets/' + website.value.seo.image + '?width=1500&height=780' : ''
-
-const head_favicon = website.value?.favicon ? config.public.linotype.backend_url + '/assets/' + website.value.favicon + '?width=250&height=250' : ''
-
-useHead({
-  title: head_title,
-  meta: [  
-    { name: 'title', content: head_title },
-    { name: 'description', content: head_description },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
-    { name: 'charset', content: 'utf-8' },
-    { name: 'apple-mobile-web-app-title', content: head_name },
-    { name: 'application-name', content: head_name },
-    { name: 'msapplication-TileColor', content: '#1e1d1d' },
-    { name: 'theme-color', content: '#1e1d1d' },
-    ...(website.value?.metas || [])
-  ],
-  link: [
-    { rel: 'canonical', href: page.value?.url || '' },
-    { rel: 'apple-touch-icon', sizes: '180x180', href: head_favicon },
-    { rel: 'icon', type: 'image/png', sizes: '32x32', href: head_favicon },
-    { rel: 'icon', type: 'image/png', sizes: '16x16', href: head_favicon },
-    { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#1e1d1d' }
-    // { rel: 'manifest', href: '/site.webmanifest' },
-  ],
-  htmlAttrs: { lang: website.value?.locale || 'en_EN' }
-})
+const head_name = computed(() => website.value?.name || '')
+const head_title = computed(() => page.value?.seo?.title || website.value?.seo?.title || website.value?.name + ' - ' + page.value?.title || '')
+const head_description = computed(() => page.value?.seo?.description || website.value?.seo?.description || '')
+const head_image = computed(() => page.value?.seo?.image ? config.public.linotype.backend_url + '/assets/' + page.value.seo.image + '?width=1500&height=780' : '' || website.value?.seo?.image ? config.public.linotype.backend_url + '/assets/' + website.value.seo.image + '?width=1500&height=780' : '')
+const head_favicon = computed(() => website.value?.favicon ? config.public.linotype.backend_url + '/assets/' + website.value.favicon + '?width=250&height=250' : '')
 
 useSeoMeta({
   title: head_title,
@@ -49,6 +23,22 @@ useSeoMeta({
   ogDescription: head_description,
   ogImage: head_image,
   twitterCard: 'summary_large_image',
+  appleMobileWebAppTitle: head_title,
+  applicationName: head_title,
+  msapplicationTileColor: '#1e1d1d',
+  themeColor: '#1e1d1d',
+})
+
+useHead({
+  htmlAttrs: { lang: website.value?.locale || 'en' },
+  link: [
+    { rel: 'canonical', href: page.value?.url || '' },
+    { rel: 'apple-touch-icon', sizes: '180x180', href: head_favicon },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: head_favicon },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', href: head_favicon },
+    { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#1e1d1d' }
+  ],
+  meta: computed(() => website.value?.metas || []),
 })
 
 const checkError = () => {
