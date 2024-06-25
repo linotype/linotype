44330dc1ee615b572a33c78d71ac405f7a04d004
YannickArmspach <yannick.armspach@gmail.com>
2024-06-25 09:02:09 +0200

    fix config

--- websites/frontend/nuxt.config.ts
+++ websites/frontend/nuxt.config.ts
@@ -16,6 +16,15 @@ export default defineNuxtConfig({
     LinotypeModule
   ],
 
+  // Default Linotype settings
+  linotype: {
+    src: '~/components/linotype',
+    env: process.env.LINOTYPE_ENV,
+    debug: process.env.LINOTYPE_DEBUG,
+    backend_url: process.env.LINOTYPE_BACKEND_URL,
+    backend_token: process.env.LINOTYPE_BACKEND_TOKEN,
+  },
+
   ui: {
     icons: ['heroicons', 'simple-icons']
   },
