f7f9d69bbbf207a27463c635aba055da3e5e944a
YannickArmspach <yannick.armspach@gmail.com>
2024-07-29 12:10:10 +0200

    nuxt module to 1.7.9

--- packages/@linotype/nuxt/src/runtime/components/linotype-template.vue
+++ packages/@linotype/nuxt/src/runtime/components/linotype-template.vue
@@ -1,20 +1,19 @@
 <template>
-  <slot />
+    <slot />
 </template>
 
 <script lang="ts" setup>
-import { computed, onMounted, watch } from "vue";
+import { onMounted, watch } from "vue";
 import { createError } from "#app"
 import { useHead, useSeoMeta } from "@unhead/vue"
 import useLinotype from "./../composables/useLinotype"
 
 const { config, website, page, error } = useLinotype();
 
-const head_name = computed(() => website.value?.name || '')
-const head_title = computed(() => page.value?.seo?.title || website.value?.seo?.title || website.value?.name + ' - ' + page.value?.title || '')
-const head_description = computed(() => page.value?.seo?.description || website.value?.seo?.description || '')
-const head_image = computed(() => page.value?.seo?.image ? config.public.linotype.backend_url + '/assets/' + page.value.seo.image + '?width=1500&height=780' : '' || website.value?.seo?.image ? config.public.linotype.backend_url + '/assets/' + website.value.seo.image + '?width=1500&height=780' : '')
-const head_favicon = computed(() => website.value?.favicon ? config.public.linotype.backend_url + '/assets/' + website.value.favicon + '?width=250&height=250' : '')
+const head_title =  page?.seo?.title || website?.seo?.title || website?.name + ' - ' + page?.title || ''
+const head_description =  page?.seo?.description || website?.seo?.description || ''
+const head_image =  page?.seo?.image ? config.public.linotype.backend_url + '/assets/' + page.seo.image + '?width=1500&height=780' : '' || website?.seo?.image ? config.public.linotype.backend_url + '/assets/' + website.seo.image + '?width=1500&height=780' : ''
+const head_favicon =  website?.favicon ? config.public.linotype.backend_url + '/assets/' + website.favicon + '?width=250&height=250' : ''
 
 useSeoMeta({
   title: head_title,
@@ -30,15 +29,15 @@ useSeoMeta({
 })
 
 useHead({
-  htmlAttrs: { lang: website.value?.locale || 'en' },
+  htmlAttrs: { lang: website?.locale || 'en' },
   link: [
-    { rel: 'canonical', href: page.value?.url || '' },
+    { rel: 'canonical', href: page?.url || '' },
     { rel: 'apple-touch-icon', sizes: '180x180', href: head_favicon },
     { rel: 'icon', type: 'image/png', sizes: '32x32', href: head_favicon },
     { rel: 'icon', type: 'image/png', sizes: '16x16', href: head_favicon },
     { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#1e1d1d' }
   ],
-  meta: computed(() => website.value?.metas || []),
+  meta: website?.metas || [],
 })
 
 const checkError = () => {
